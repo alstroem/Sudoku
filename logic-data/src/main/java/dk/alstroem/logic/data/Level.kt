6cde7cd034c08243baa5994a296d1cfabf5d97e7
Andreas <alstroem1992@gmail.com>
2022-06-14 22:29:19 +0200

    Minor level adjustment

--- logic-data/src/main/java/dk/alstroem/logic/data/Level.kt
+++ logic-data/src/main/java/dk/alstroem/logic/data/Level.kt
@@ -2,6 +2,6 @@ package dk.alstroem.logic.data
 
 sealed class Level(val providedDigits: Int) {
     object Easy : Level(30)
-    object Medium : Level(25)
-    object Hard : Level(21)
+    object Medium : Level(26)
+    object Hard : Level(23)
 }
